813b600595ca9565f4e6a7822ff19a58b83dc72a
kkkuznetsov <kirillkuznetsov00000@yandex.ru>
2025-09-26 17:09:13 +0300

    task2 v1.0 without tests

---
+++ L_1/task_2/include/prime.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdbool.h>
+#include <math.h>
+
+bool isXPrime(const int x);
+int nthPrime(const int n);
---
+++ L_1/task_2/src/prime.c
@@ -0,0 +1,20 @@
+#include "../include/prime.h"
+
+bool isXPrime(const int x){
+    if (x < 2) return false;
+    if (x == 2) return true;
+    if (x % 2 == 0) return false;
+    for (int i = 3; i <= sqrt(x); i += 2) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+int nthPrime(const int n){
+    int index = 0, res = 2;
+    while (index != n){
+        if (isXPrime(res)) index++;
+        if (index != n) res++;
+    }
+    return res;
+}
